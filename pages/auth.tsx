e6e5a92a87158fd4f23a4b73a18e382977613a77
Ricardo Dias <ricardodias.dev@gmail.com>
2023-04-09 15:34:17 -0300

    Autenticação com Google e GitHub

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -4,6 +4,9 @@ import { signIn } from "next-auth/react";
 import { useCallback, useState } from "react";
 import { useRouter } from "next/router";
 
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
 const Auth = () => {
   const router = useRouter();
 
@@ -40,7 +43,7 @@ const Auth = () => {
         name,
         password,
       });
-      login()
+      login();
     } catch (error) {
       console.log(error);
     }
@@ -118,6 +121,40 @@ const Auth = () => {
             >
               {variant === "login" ? "Login" : "Sign up"}
             </button>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div
+                onClick={() => signIn("google", { callbackUrl: "/" })}
+                className="
+                  w-10 
+                  h-10 
+                  bg-white 
+                  rounded-full 
+                  flex items-center 
+                  justify-center 
+                  cursor-pointer 
+                  hover:opacity-80 
+                  transition
+                "
+              >
+                <FcGoogle size={30} />
+              </div>
+              <div
+                onClick={() => signIn("github", { callbackUrl: "/" })}
+                className="
+                  w-10 
+                  h-10 
+                  bg-white 
+                  rounded-full 
+                  flex items-center 
+                  justify-center 
+                  cursor-pointer 
+                  hover:opacity-80 
+                  transition
+                "
+              >
+                <FaGithub size={30} />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-12">
               {variant === "login"
                 ? "First time using Netflix?"
