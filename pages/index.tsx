054b4c40fea0bb836a21f6fef22c0ce954a9235f
Ricardo Dias <ricardodias.dev@gmail.com>
2023-04-10 11:38:46 -0300

    Usando o useCurrentUser para mostrar o email do usuário

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
 
@@ -17,9 +18,11 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+  const {data: user} = useCurrentUser()
   return (
     <>
       <h1 className="text-white text-3xl font-bold underline">NETFLIX CLONE</h1>
+      <p className="text-white">Logged in as : {user?.email}</p>
       <button className="h-10 w-full bg-white" onClick={() => signOut()}>
         Logout!
       </button>
