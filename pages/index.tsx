220cf20c0867c8447670edc6b14caf81acf48c5b
Ricardo Dias <ricardodias.dev@gmail.com>
2023-04-10 11:35:50 -0300

    Protegendo a rota (verifica se tem uma sessão ativa)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,28 @@
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
   return (
     <>
       <h1 className="text-white text-3xl font-bold underline">NETFLIX CLONE</h1>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+        Logout!
+      </button>
     </>
   );
 }
