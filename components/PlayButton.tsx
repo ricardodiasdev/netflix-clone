a1a0054eadff8176ea4f76189800e9a2e5979263
Ricardo Dias <ricardodias.dev@gmail.com>
2023-04-13 21:13:47 -0300

    Criando o componente PlayButton

---
+++ components/PlayButton.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { BsFillPlayFill } from "react-icons/bs";
+
+import { useRouter } from "next/router";
+
+interface PlayButtonProps {
+  movieId: string;
+}
+
+const PlayButton: React.FC<PlayButtonProps> = ({ movieId }) => {
+  const router = useRouter();
+  return (
+    <button
+      onClick={() => router.push(`/watch/${movieId}`)}
+      className="
+    flex 
+    w-auto
+    flex-row
+    items-center 
+    rounded-md
+    bg-white 
+    px-2
+    py-1
+    text-xs 
+    font-semibold
+    transition
+    hover:bg-neutral-300
+    md:px-4
+    md:py-2
+    lg:text-lg
+  "
+    >
+      <BsFillPlayFill size={25} className="mr-1" />
+      Play
+    </button>
+  );
+};
+
+export default PlayButton;
