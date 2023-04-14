59302ee0b42b3e119fe2aec872b8c6a9e365b3eb
Ricardo Dias <ricardodias.dev@gmail.com>
2023-04-13 21:16:39 -0300

    Atualizando o serverAuth

--- libs/serverAuth.ts
+++ libs/serverAuth.ts
@@ -1,24 +1,24 @@
 import { NextApiRequest } from "next";
 import { getSession } from "next-auth/react";
 
-import prismadb from "@/libs/prismadb"
+import prismadb from "@/libs/prismadb";
 
 const serverAuth = async (req: NextApiRequest) => {
-    const session = await getSession({req})
-    if(!session?.user?.email) {
-        throw new Error("Not signed in")
-    }
-    const currentUser = await prismadb.user.findUnique({
-        where: {
-            email: session.user.email
-        }
-    })
+  const session = await getSession({ req });
+  if (!session?.user?.email) {
+    throw new Error("Not signed in");
+  }
+  const currentUser = await prismadb.user.findUnique({
+    where: {
+      email: session.user.email,
+    },
+  });
 
-    if(!currentUser) {
-        throw new Error('Not signed in')
-    }
+  if (!currentUser) {
+    throw new Error("Not signed in");
+  }
 
-    return {currentUser}
-}
+  return { currentUser };
+};
 
-export default serverAuth
+export default serverAuth;
